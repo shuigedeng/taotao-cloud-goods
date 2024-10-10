9c27152b5ed46ed8a37aaaef6bc55a3abb9ce8d0
dengtao <981376577@qq.com>
2024-10-10 19:19:21 +0800

    xx

--- taotao-cloud-goods-assembly/src/main/java/com/taotao/cloud/goods/TaoTaoCloudGoodsApplication.java
+++ taotao-cloud-goods-assembly/src/main/java/com/taotao/cloud/goods/TaoTaoCloudGoodsApplication.java
@@ -17,24 +17,20 @@
 package com.taotao.cloud.goods;
 
 import com.taotao.boot.core.startup.StartupSpringApplication;
-import com.taotao.boot.data.jpa.extend.JpaExtendRepositoryFactoryBean;
 import com.taotao.boot.web.annotation.TaoTaoBootApplication;
 import com.taotao.cloud.bootstrap.annotation.TaoTaoCloudApplication;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * 系统管理中心
  * <p>异常原因：
- *
+ * <p>
  * 自从 JDK9 中引入了模块化功能后，再到 JDK17，对于包扫描和反射的权限控制更加的严格。常见的库比如（Spring）大量用到包扫描和反射，所以常出现此错误。
- *
+ * <p>
  * 解决方案：
- *
+ * <p>
  * 一个粗暴的解决办法是将没开放的 module 强制对外开放，即保持和 Java9 之前的版本一致。
- *
- * --add-exports 导出包，意味着其中的所有公共类型和成员都可以在编译和运行时访问。
- * --add-opens 打开包，意味着其中的所有类型和成员（不仅是公共类型）都可以在运行时访问。
+ * <p>
+ * --add-exports 导出包，意味着其中的所有公共类型和成员都可以在编译和运行时访问。 --add-opens 打开包，意味着其中的所有类型和成员（不仅是公共类型）都可以在运行时访问。
  * 主要区别在于 --add-opens 允许 “深度反射”，即非公共成员的访问，才可以调用 setAccessible(true)
  *
  * <pre class="code">
@@ -57,10 +53,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @version 2022.03
  * @since 2020/11/30 下午3:33
  */
-@MapperScan(basePackages = {"com.taotao.cloud.goods.infrastructure.persistent.*.mapper"})
-@EnableJpaRepositories(
-	basePackages = {"com.taotao.cloud.goods.infrastructure.persistent.*.repository.inf"},
-	repositoryFactoryBeanClass = JpaExtendRepositoryFactoryBean.class)
+//@MapperScan(basePackages = {"com.taotao.cloud.goods.infrastructure.persistent.*.mapper"})
+//@EnableJpaRepositories(
+//	basePackages = {"com.taotao.cloud.goods.infrastructure.persistent.*.repository.inf"},
+//	repositoryFactoryBeanClass = JpaExtendRepositoryFactoryBean.class)
 //@ComponentScan(basePackages = {
 //	"com.taotao.cloud.goods.biz.repository.cls"
 //} )
