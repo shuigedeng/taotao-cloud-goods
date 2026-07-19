5ddf0792ed711c553cfc9d561e828cf2803d2591
shuigedeng <981376577@qq.com>
2026-07-19 14:37:38 +0800

    开启2026.08

--- taotao-cloud-goods-infrastructure/src/main/java/com/taotao/cloud/goods/infrastructure/event/consumer/kafka/GoodsKafkaConsumer.java
+++ taotao-cloud-goods-infrastructure/src/main/java/com/taotao/cloud/goods/infrastructure/event/consumer/kafka/GoodsKafkaConsumer.java
@@ -1,42 +1,37 @@
 package com.taotao.cloud.goods.infrastructure.event.consumer.kafka;
 
-import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
 import com.taotao.boot.common.utils.log.LogUtils;
 import com.taotao.boot.mq.common.base.MqConsumerBase;
-import com.taotao.boot.mq.common.consumer.Acknowledgement;
 import com.taotao.cloud.goods.application.dto.goods.command.NotifyGoodsCommand;
 import com.taotao.cloud.goods.application.service.command.GoodsCommandService;
-import com.xxl.job.core.context.XxlJobContext;
-import com.xxl.job.core.context.XxlJobHelper;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ConsumerAwareListenerErrorHandler;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @AllArgsConstructor
 public class GoodsKafkaConsumer extends MqConsumerBase {
 
 	private final GoodsCommandService goodsCommandService;
 
 	@KafkaListener(
-		topics = "xxx",
-		groupId = "xx-xxx-xx",
-		concurrency = "3",
-		errorHandler = "kafkaErrorHandler",
-		containerFactory = "manualKafkaContainerFactory"
+		topics = "GoodsTopic",
+		groupId = "taotao-cloud-GoodsTopic",
+		concurrency = "3"
+//		errorHandler = "kafkaErrorHandler",
+//		containerFactory = "manualKafkaContainerFactory"
 	)
-	public void listenMsg( String msg,
-		@Header(KafkaHeaders.RECEIVED_KEY) String key,
-		Acknowledgement ack ) {
+	public void listenMsg(
+//		@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+//		@Header(KafkaHeaders.RECEIVED_KEY) String key,
+//		@Payload String msg,
+		ConsumerRecord<String, String> record,
+		Acknowledgment ack ) {
 
 		try {
+			String msg = record.value();
 			NotifyGoodsCommand notifyGoodsCommand = from(msg, NotifyGoodsCommand.class);
 
 			goodsCommandService.handleKafkaNotify(notifyGoodsCommand);
@@ -49,21 +44,21 @@ public class GoodsKafkaConsumer extends MqConsumerBase {
 		}
 	}
 
-	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> manualKafkaContainerFactory(
-		ConsumerFactory<String, String> consumerFactory ) {
-		ConcurrentKafkaListenerContainerFactory<String, String> container = new ConcurrentKafkaListenerContainerFactory<>();
-		container.setConsumerFactory(consumerFactory);
-		container.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-		return container;
-	}
+//	@Bean
+//	public ConcurrentKafkaListenerContainerFactory<String, String> manualKafkaContainerFactory(
+//		ConsumerFactory<String, String> consumerFactory ) {
+//		ConcurrentKafkaListenerContainerFactory<String, String> container = new ConcurrentKafkaListenerContainerFactory<>();
+//		container.setConsumerFactory(consumerFactory);
+//		container.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//		return container;
+//	}
 
-	@Bean
-	public ConsumerAwareListenerErrorHandler kafkaErrorHandler() {
-		return ( message, exception, consumer ) -> {
-			//可以选择重试 进入死信队列
-			return null;
-		};
-	}
+//	@Bean
+//	public ConsumerAwareListenerErrorHandler kafkaErrorHandler() {
+//		return ( message, exception, consumer ) -> {
+//			//可以选择重试 进入死信队列
+//			return null;
+//		};
+//	}
 
 }
