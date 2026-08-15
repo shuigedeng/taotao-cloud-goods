1f4785e9484f83db3b947700435ab29ebd8d2493
shuigedeng <981376577@qq.com>
2026-08-15 09:59:40 +0800

    开启2026.09

---
+++ taotao-cloud-goods-application/src/main/java/com/taotao/cloud/goods/application/support/validator/BrandCommandValidator.java
@@ -0,0 +1,12 @@
+package com.taotao.cloud.goods.application.support.validator;
+
+/**
+ * BrandCommandValidator 类
+ *
+ * @author shuigedeng
+ * @version 2022.05
+ * @since 2026/8/15
+ */
+public class BrandCommandValidator {
+
+}
