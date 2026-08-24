f448b520ea7288a9d61925a4da1de2154fdd8ff4
shuigedeng <981376577@qq.com>
2026-08-24 19:53:47 +0800

    开启2026.09

--- taotao-cloud-goods-application/src/main/java/com/taotao/cloud/goods/application/flow/precredit/PreCreditFlow.java
+++ taotao-cloud-goods-application/src/main/java/com/taotao/cloud/goods/application/flow/precredit/PreCreditFlow.java
@@ -9,41 +9,54 @@ import org.springframework.stereotype.Service;
 @Service
 public class PreCreditFlow implements FlowDsl<PreCreditContext, GoodsResult> {
 
-	public void start(){
+	static void main() {
+		PreCreditFlow preCreditFlow = new PreCreditFlow();
+		preCreditFlow.start();
+	}
+	public  void start(){
 		PreCreditContext preCreditContext = new PreCreditContext();
 		preCreditContext.setGoodsResult(GoodsResultBuilder.builder().build());
 		GoodsResult result = this.definition().execute(preCreditContext).getResult();
 	}
 	@Override
 	public FlowDefinition<PreCreditContext, GoodsResult> definition() {
-		return start("xxxx")
-			.step("xxx").invoke(this::test)
-			.step("xxx").predicate(ctx -> ctx.isCredit()).invoke(this::test1)
-			.step("xxx").end(this::test3)
-			.build();
+//		return start("xxxx")
+//			.step("xxx").invoke(this::test)
+//			.step("xxx").predicate(ctx -> ctx.isCredit()).invoke(this::test1)
+//			.step("xxx").end(this::test3)
+//			.build();
 
-		return start("xxxx")
-			.step("xxx").invoke(this::test)
-			.step("xxx").predicate(ctx -> ctx.isCredit()).invoke(this::test1)
-			.step("xx").conditional()
-			.when("xx", ctx-> ctx.isCredit()).step("xx").invoke(this::test).close()
-			.otherwise("sdfas").step("xx").invoke(this::test).close()
+		return start("start")
+			.step("step1").invoke(this::step1)
+			.step("steppredicate").predicate(ctx -> !ctx.isCredit()).invoke(this::steppredicate)
+			.step("steppconditional").conditional()
+			.when("steppconditional1", ctx-> !ctx.isCredit()).step("steppconditional2").invoke(this::steppconditional2).close()
+			.otherwise("steppotherwise").step("steppotherwise1").invoke(this::steppotherwise1).close()
 			.finish()
-			.step("xx")
-			.end(this::test3)
+			.step("stepend")
+			.end(this::stepend)
 			.build();
 	}
 
-	public void test(PreCreditContext  creditContext) {
-
+	public void step1(PreCreditContext  creditContext) {
+		System.out.println("step1");
 	}
 
 
-	public void test1(PreCreditContext  creditContext) {
-
+	public void steppredicate(PreCreditContext  creditContext) {
+		System.out.println("steppredicate");
 	}
 
-	public GoodsResult test3(PreCreditContext  creditContext) {
+	public void steppconditional2(PreCreditContext  creditContext) {
+		System.out.println("steppconditional2");
+	}
+
+	public void steppotherwise1(PreCreditContext  creditContext) {
+		System.out.println("steppotherwise1");
+	}
+
+	public GoodsResult stepend(PreCreditContext  creditContext) {
+		System.out.println("stepend");
 	return creditContext.getGoodsResult();
 	}
 }
