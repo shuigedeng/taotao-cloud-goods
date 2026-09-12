cb872fca349ca0705a06636af09ade4822d03da4
shuigedeng <981376577@qq.com>
2026-09-12 15:46:09 +0800

    开启2026.10

--- taotao-cloud-goods-application/src/main/java/com/taotao/cloud/goods/application/flow/flow/callback/CallbackConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.cloud.goods.application.flow.flow.callback;
-
-public interface CallbackConsumer<T> {
-    /**
-     * @param t
-     */
-    void consumer(T t);
-}
--- taotao-cloud-goods-application/src/main/java/com/taotao/cloud/goods/application/flow/flow/treeflow/channel/ChannelType.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.cloud.goods.application.flow.flow.treeflow.channel;
-
-public interface ChannelType {
-    /**
-     * MANUAL
-     */
-    String MANUAL = "manual";
-}
